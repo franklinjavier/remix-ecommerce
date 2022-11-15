eaa298c85f7a6e3cc2378d347551aa261092fc3f
Franklin Javier <franklinjalves@gmail.com>
2022-11-15 00:54:19 +0000

    chore: vercel build target

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,8 @@
-/** @type {import('@remix-run/dev').AppConfig} */
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
 module.exports = {
-  ignoredRouteFiles: ['**/.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  serverBuildTarget: 'vercel',
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  ignoredRouteFiles: ['.*'],
 }
