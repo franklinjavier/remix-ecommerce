5cfacb9d15031b6541d535d6ca90e2140c5fc012
Claudio Alcantara <claudiomar.alcantara@gmail.com>
2022-12-23 13:18:32 -0300

    fix: type search options

--- app/models/search.server.ts
+++ app/models/search.server.ts
@@ -1,12 +1,12 @@
 import algoliasearch from 'algoliasearch'
 
 import type { Product } from './product'
-import type { AlgoliaSearchOptions } from 'algoliasearch'
+import type { SearchOptions } from '@algolia/client-search'
 
 const client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76')
 const algolia = client.initIndex('instant_search')
 
-export async function search(query: string, options: AlgoliaSearchOptions) {
+export async function search(query: string, options: SearchOptions) {
   return await algolia.search(query, options)
 }
 export async function getProduct(sku: string) {
